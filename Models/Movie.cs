16e4c7c068c46ca5a7d23fd584addec2ebd175e5
NimaZare <NimaMicrosoft@yahoo.com>
2023-06-06 10:36:25 +0330

    update annotations in movie class

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,27 +8,19 @@ public class Movie
 {
     public int Id { get; set; }
 
-    [StringLength(60, MinimumLength = 3)]
-    [Required]
+    [StringLength(60, MinimumLength = 3), Required]
     public string? Title { get; set; }
 
-    [Display(Name = "Release Date")]
-    [DataType(DataType.Date)]
+    [Display(Name = "Release Date"), DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     public DateTime ReleaseDate { get; set; }
 
-    [Range(1, 100)]
-    [DataType(DataType.Currency)]
-    [Column(TypeName = "decimal(18, 2)")]
+    [Range(1, 100), DataType(DataType.Currency), Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
-    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-    [Required]
-    [StringLength(30)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), StringLength(30), Required]
     public string? Genre { get; set; }
 
-    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-    [StringLength(5)]
-    [Required]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5), Required]
     public string? Rating { get; set; }
 }
